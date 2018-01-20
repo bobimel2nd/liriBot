b2ada896a7875d9308afc887a79defcd083a5521
bobimel2nd <31457563+bobimel2nd@users.noreply.github.com>
2018-01-20 09:55:18 -0800

    Version 1.0.5

--- liri.js
+++ liri.js
@@ -1,4 +1,6 @@
 logInit();
+var dashes = "=".repeat(100);
+var indent = 0;
 
 var args = process.argv;
 require("dotenv").config();
@@ -6,56 +8,45 @@ var all = require("./keys.js");
 var twitter = all.twitter;
 var spotify = all.spotify;
 var readyForNextCommand;
+var subJSONs = [];
 
-DoCommands();
 
-function logInit() {
-	var fs = require('fs');
-	if (fs.existsSync('LastRun.log')) fs.unlinkSync('LastRun.log');
-}
+DoCommands(args[2], args[3]);
 
-function logText(txt, obj) {
-	var timestamp = require('time-stamp');
- 	var now = timestamp('YYYY/MM/DD HH:mm:ss');
- 	var fs = require('fs');
-	console.log(txt);
-	fs.appendFileSync('AllRun.log', now.toString() + " - " + txt + '\n');
-	fs.appendFileSync('LastRun.log', now.toString() + " - " + txt + '\n');
-	if (obj) {
-		console.log(obj);
-		fs.appendFileSync('AllRun.log', obj);
-		fs.appendFileSync('LastRun.log', obj);
-	}
-} 
-
-function DoCommands() {
-	logText('================================================================================================');
-	switch (args[2]) {
+function DoCommands(cmd, val) {
+	cmd = cmd.toLowerCase();
+	val = val.toLowerCase();
+	logText(dashes);
+	switch (cmd) {
 		case 'my-tweets':
-			if (args.length < 4) args.push("20");
-			twitterTweets();
+			if (val === undefined) val=20;
+			twitterTweets(val);
 			break;
 		case 'spotify-this-song':
-			if (args.length < 4) args.push("Rapid Roy");
-			spotifySong();
+			if (val === undefined) val="rapid roy";
+			spotifySong(val);
 			break;
 		case 'movie-this':
-			if (args.length < 4) args.push("2001: A Space Odyssey");
-			responseOMDB();
+			if (val === undefined) val="2001: a space odyssey";
+			responseOMDB(val);
 			break;
 		case 'do-what-it-says':
-			if (args.length < 4) args.push("Random.txt");
-			fsDoWhatItSays();
+			if (val === undefined) val="random.txt";
+			if (subJSONs.indexOf(val) !== -1) {
+				logText("Canceled recursive call do-what-it-says " + val)
+				break;
+			}
+			fsDoWhatItSays(val);
 			break;
 		default:
-			logText('Unknown Command:', args);
+			logText('Unknown Command:', cmd);
 			break;
 	}
 }
 
-function twitterTweets() {
+function twitterTweets(val) {
 	readyForNextCommand = false;
-	let max = parseInt(args[3]);
+	let max = parseInt(val);
 	logText("My Twitter Tweets (" + max + " max)");
 	var Twitter = require('twitter');
 	var client = new Twitter({
@@ -64,7 +55,7 @@ function twitterTweets() {
 		access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
 		access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 	});
-	client.get('statuses/user_timeline', {screen_name: 'BoltUpBob'}, function(error, tweets, response) {
+	client.get('statuses/user_timeline', {screen_name: 'BoltUpBob'}, (error, tweets, response) => {
 	 	if (error) throw error;
 	 	if (max > tweets.length) max = tweets.length;
 	 	for (var i=0; i<max; i++) {
@@ -74,16 +65,15 @@ function twitterTweets() {
 	});
 }
 
-function spotifySong() {
+function spotifySong(val) {
 	readyForNextCommand = false;
-	logText("Spotify Song Info for '" + args[3] + "'");
+	logText("Spotify Song Info for '" + val + "'");
 	var Spotify = require('node-spotify-api');
 	var spotify = new Spotify({
 		id: process.env.SPOTIFY_ID,
 		secret: process.env.SPOTIFY_SECRET
 	});
-
-	spotify.search({ type: 'track', query: args[3] }, function(error, data) {
+	spotify.search({ type: 'track', query: val }, (error, data) => {
 		if (error) throw error;
 		var firstTrack = data.tracks.items[0];
 		logText("Artist:  " + firstTrack.artists[0].name);
@@ -95,14 +85,14 @@ function spotifySong() {
 	});
 }
 
-function responseOMDB() {
+function responseOMDB(val) {
 	readyForNextCommand = false;
-	logText("OMDB Movie Info for '" + args[3] + "'");
+	logText("OMDB Movie Info for '" + val + "'");
 	var request = require('request');
-	request("http://www.omdbapi.com/?s=" + args[3] + "&plot=short&r=json&apikey=trilogy", function (error, response, body) {
+	request("http://www.omdbapi.com/?s=" + val + "&plot=short&r=json&apikey=trilogy", (error, response, body) => {
 	 	if (error) throw error;
 		var firstMovie = JSON.parse(body).Search[0];
-		request("https://www.omdbapi.com/?i=" + firstMovie.imdbID + "&y=&plot=Full&apikey=trilogy", function (error, response, body) {
+		request("https://www.omdbapi.com/?i=" + firstMovie.imdbID + "&y=&plot=Full&apikey=trilogy", (error, response, body) => {
 		 	if (error) throw error;
 			var zMovie = JSON.parse(body);
 			var Ratings = ""
@@ -125,29 +115,54 @@ function responseOMDB() {
 			logText("Poster:   " + zMovie.Poster);
 			logText("Plot:     " + zMovie.Plot);
 			readyForNextCommand = true;
-		});	
-	});	
+		});
+	});
 }
 
-function fsDoWhatItSays() {
-	logText('do-what-it-says in ' + args[3]);
+function fsDoWhatItSays(val) {
+	logText('do-what-it-says in ' + val);
+	indent++;
+	subJSONs.push(val);
 	var fs = require('fs');
+	var nxtCommand = JSON.parse(fs.readFileSync(val).toString());
 	readyForNextCommand = true;
-	doNextCommand(JSON.parse(fs.readFileSync(args[3]).toString()));
+	doNextCommand(nxtCommand);
 }
 
 function doNextCommand(data) {
 	let dataJSON = data;
-	if (dataJSON.length > 0) {
-		args[2] = dataJSON[0].Action;
-		args[3] = dataJSON[0].Value;
-		dataJSON.splice(0, 1);
-		let waitForComplete = setInterval(function () {
-			if(readyForNextCommand) {
-				clearInterval(waitForComplete);
-				DoCommands();
+	let waitForComplete = setInterval( () => {
+		if(readyForNextCommand) {
+			clearInterval(waitForComplete);
+			if (dataJSON.length === 0) {
+				logText(dashes);
+				indent--;
+				logText('do-what-it-says in ' + subJSONs.pop() + " completed.");
+			} else {
+				DoCommands(dataJSON[0].Action, dataJSON[0].Value);
+				dataJSON.splice(0, 1);
 				doNextCommand(dataJSON);
 			}
-		}, 100);
+		}
+	}, 100);
+}
+
+function logInit() {
+	var fs = require('fs');
+	if (fs.existsSync('LastRun.log')) fs.unlinkSync('LastRun.log');
+}
+
+function logText(txt, obj) {
+	var timestamp = require('time-stamp');
+ 	var now = timestamp('YYYY/MM/DD HH:mm:ss');
+ 	var fs = require('fs');
+ 	txt = " ".repeat(indent*4) + txt
+	console.log(txt);
+	fs.appendFileSync('AllRun.log', now.toString() + " - " + txt + '\n');
+	fs.appendFileSync('LastRun.log', now.toString() + " - " + txt + '\n');
+	if (obj) {
+		console.log(obj);
+		fs.appendFileSync('AllRun.log', obj);
+		fs.appendFileSync('LastRun.log', obj);
 	}
 }
